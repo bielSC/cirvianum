703bb8194f75da659943be14cb1ea1942b68da0c
bielSC <bielprogramming@gmail.com>
2019-09-20 18:44:40 +0200

    Creacio del fitxer de suma en java

---
+++ suma.java
@@ -0,0 +1,8 @@
+int numero1 = 0;
+int numero2 = 0;
+
+System.out.println("introdueix el primer numero");
+numero1 = reader.nextInt();
+
+System.out.println("introdueix el seguent numero");
+numero2 = reader.nextInt()
